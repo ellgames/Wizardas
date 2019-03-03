d4fa088b4f9770afba1b65093626805c52823994
Doo <doo11gms@gmail.com>
2019-03-03 12:04:35 +0900

    add

---
+++ Wizardas/Assets/Wizardas/Scripts/Camera/FreeLookSettings.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace EllGames.Wiz.Camera
+{
+    [System.Serializable]
+    public class FreeLookSettings
+    {
+        [SerializeField] bool m_FreeLookLock;
+        public bool FreeLookLock
+        {
+            get { return m_FreeLookLock; }
+            set { m_FreeLookLock = value; }
+        }
+
+        [SerializeField] float m_FreeLookSpeed = 1.0f;
+        public float FreeLookSpeed
+        {
+            get { return m_FreeLookSpeed; }
+        }
+
+        [SerializeField] float m_RotationMinX = 0.0f;
+        public float RotationMinX
+        {
+            get { return m_RotationMinX; }
+        }
+
+        [SerializeField] float m_RotationMaxX = 80.0f;
+        public float RotationMaxX
+        {
+            get { return m_RotationMaxX; }
+        }
+    }
+}
---
+++ Wizardas/Assets/Wizardas/Scripts/Camera/ZoomSettings.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace EllGames.Wiz.Camera
+{
+    [System.Serializable]
+    public class ZoomSettings
+    {
+        [SerializeField] bool m_ZoomLock;
+        public bool ZoomLock
+        {
+            get { return m_ZoomLock; }
+            set { m_ZoomLock = value; }
+        }
+
+        [SerializeField] float m_ZoomSpeed = 10.0f;
+        public float ZoomSpeed
+        {
+            get { return m_ZoomSpeed; }
+        }
+
+        [SerializeField] float m_ViewRangeMin = 2.5f;
+        public float ViewRangeMin
+        {
+            get { return m_ViewRangeMin; }
+        }
+
+        [SerializeField] float m_ViewRangeMax = 15.0f;
+        public float ViewRangeMax
+        {
+            get { return m_ViewRangeMax; }
+        }
+    }
+}
