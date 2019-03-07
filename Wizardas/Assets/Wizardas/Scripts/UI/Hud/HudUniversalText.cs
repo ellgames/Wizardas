785d8b946df27d7764b502672bcf5244628e9181
Doo <doo11gms@gmail.com>
2019-03-07 17:43:06 +0900

    [add]HudUniversalText

---
+++ Wizardas/Assets/Wizardas/Scripts/UI/Hud/HudUniversalText.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+using UnityEngine.Events;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+
+namespace EllGames.Wiz.UI.Hud
+{
+    [RequireComponent(typeof(Text))]
+    public class HudUniversalText : HudComponent
+    {
+        [Title("Required")]
+        [OdinSerialize, Required] Profile.SystemProfile m_SystemProfile;
+
+        [Title("Settings")]
+        [OdinSerialize] string m_TextEng = "Text";
+        [OdinSerialize] string m_TextJpn = "テキスト";
+
+        Text m_Text;
+
+        private void Awake()
+        {
+            m_Text = GetComponent<Text>();
+
+            switch (m_SystemProfile.Language)
+            {
+                case Meta.LANGUAGE.English:
+                    m_Text.text = m_TextEng;
+                    break;
+                case Meta.LANGUAGE.Japanese:
+                    m_Text.text = m_TextJpn;
+                    break;
+            }
+        }
+    }
+}
