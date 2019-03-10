3deb3aee9fac94225fb4cfab2eef48de3b254681
Doo <doo11gms@gmail.com>
2019-03-11 03:29:22 +0900

    [hotfix]ビルドするとプレイヤー名が保存できなくなるバグを修正

---
+++ Wizardas/Assets/Wizardas/Scripts/Develop/Debug/DebugCanvas2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+using UnityEngine.Events;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+
+namespace EllGames.Wiz.Develop.Debug
+{
+    public class DebugCanvas2 : SerializedMonoBehaviour
+    {
+        [OdinSerialize] Profile.PlayerProfile m_PlayerProfile;
+        [OdinSerialize] Text m_Text;
+
+        private void Update()
+        {
+            m_Text.text = m_PlayerProfile.PlayerName;
+        }
+    }
+}
