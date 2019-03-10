68c8c970f56468753ea1f0369c100712c75218d1
Doo <doo11gms@gmail.com>
2019-03-10 15:08:40 +0900

    [add]Gameシーンに入った時にカウントダウンが入るようになりました

---
+++ Wizardas/Assets/Wizardas/Scripts/UI/Hud/HudCountDownText.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+using UnityEngine.Events;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+
+namespace EllGames.Wiz.UI.Hud
+{
+    [RequireComponent(typeof(Text))]
+    public class HudCountDownText : HudComponent
+    {
+        [Title("Required")]
+        [OdinSerialize] Utility.Timer m_Timer;
+
+        [Title("Settings")]
+        [OdinSerialize] bool m_Fading = true;
+        [OdinSerialize] int m_StartFontSize;
+        [OdinSerialize] int m_EndFontSize;
+
+        Text m_Text;
+
+        private void Awake()
+        {
+            m_Text = GetComponent<Text>();    
+        }
+
+        private void Update()
+        {
+            m_Text.text = ((int)m_Timer.Seconds).ToString();
+            m_Text.fontSize = (int)Mathf.Lerp(m_StartFontSize, m_EndFontSize, 1f - (m_Timer.Seconds - (int)m_Timer.Seconds));
+
+            if (m_Fading)
+            {
+                var currentColor = m_Text.color;
+                currentColor.a = m_Timer.Seconds - (int)m_Timer.Seconds;
+                m_Text.color = currentColor;
+            }
+        }
+    }
+}
---
+++ Wizardas/Assets/Wizardas/Scripts/UI/Hud/HudFireText.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+using UnityEngine.Events;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+
+namespace EllGames.Wiz.UI.Hud
+{
+    [RequireComponent(typeof(Text))]
+    public class HudFireText : HudComponent
+    {
+        [Title("Settings")]
+        [OdinSerialize] float m_Duration = 1f;
+        [OdinSerialize] int m_StartFontSize;
+        [OdinSerialize] int m_EndFontSize;
+
+        Text m_Text;
+        float m_TimeElapsed;
+
+        private void Awake()
+        {
+            m_Text = GetComponent<Text>();
+        }
+
+        private void Update()
+        {
+            /*
+            m_TimeElapsed += Time.deltaTime;
+
+            var currentColor = m_Text.color;
+            currentColor.a = 1f - m_TimeElapsed / m_Duration;
+            m_Text.color = currentColor;
+
+            m_Text.fontSize = (int)Mathf.Lerp(m_StartFontSize, m_EndFontSize, m_TimeElapsed / m_Duration);
+            */
+
+            /* 半分待機後にフェードアウトしていくパターン*/
+            m_TimeElapsed += Time.deltaTime;
+
+            if (m_TimeElapsed >= m_Duration / 2)
+            {
+                var currentColor = m_Text.color;
+                currentColor.a = 1f - m_TimeElapsed / m_Duration;
+                m_Text.color = currentColor;
+
+                m_Text.fontSize = (int)Mathf.Lerp(m_StartFontSize, m_EndFontSize, ((m_TimeElapsed - m_Duration / 2) * 2) / m_Duration);
+            }
+            
+        }
+    }
+}
