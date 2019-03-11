be5353ac715e76c303be03f4724c647493b15cba
Doo <doo11gms@gmail.com>
2019-03-11 22:06:57 +0900

    [update]マイルームで名前変更中にキャンセルボタンを押すと変更をキャンセルできるようになりました

---
+++ Wizardas/Assets/Wizardas/Scripts/InputManagement/PressCancelDownHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+using UnityEngine.Events;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+
+namespace EllGames.Wiz.InputManagement
+{
+    public class PressCancelDownHandler : SerializedMonoBehaviour
+    {
+        [Title("Required")]
+        [OdinSerialize, Required] Config.KeyConfig m_KeyConfig;
+
+        [Title("Callback")]
+        [OdinSerialize] UnityEvent m_OnPressCancel = new UnityEvent();
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(m_KeyConfig.CancelKey)) m_OnPressCancel.Invoke();
+        }
+    }
+}
