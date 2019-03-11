4b6c65b288cd221e687182f9d511ce1d571a8a01
Doo <doo11gms@gmail.com>
2019-03-11 19:47:18 +0900

    [update]camera プレイヤーを追従するカメラに変更

---
+++ Wizardas/Assets/Wizardas/Scripts/Camera/SmoothlyChasingCamera.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+using UnityEngine.Events;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+
+namespace EllGames.Wiz.Camera
+{
+    [RequireComponent(typeof(UnityEngine.Camera))]
+    public class SmoothlyChasingCamera : SerializedMonoBehaviour
+    {
+        [Title("Settings")]
+        [OdinSerialize] Transform m_Chased;
+        [OdinSerialize] Transform m_Focused;
+        [OdinSerialize] float m_Speed = 0.125f;
+
+        [Title("Read Only")]
+        [OdinSerialize, ReadOnly] Vector3 m_Offset;
+
+        [Button("Focus")]
+        public void Focus()
+        {
+            transform.LookAt(m_Focused);
+        }
+
+        private void Awake()
+        {
+            Focus();
+            m_Offset = transform.position - m_Chased.position;
+        }
+
+        private void FixedUpdate()
+        {
+            var desiredPosition = m_Chased.position + m_Offset;
+            var smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, m_Speed);
+            transform.position = smoothedPosition;
+        }
+    }
+}
