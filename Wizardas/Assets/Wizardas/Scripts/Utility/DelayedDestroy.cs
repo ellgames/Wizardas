4e61eb03fa578d3258d5c024a32b99df5ca70699
Doo <doo11gms@gmail.com>
2019-03-11 18:41:42 +0900

    [add]monster spawn effect

---
+++ Wizardas/Assets/Wizardas/Scripts/Utility/DelayedDestroy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+using UnityEngine.Events;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+
+namespace EllGames.Wiz.Utility
+{
+    public class DelayedDestroy : SerializedMonoBehaviour
+    {
+        [Title("Settings")]
+        [OdinSerialize] float m_Duration = 1f;
+
+        private void Awake()
+        {
+            StartCoroutine(DelayedDestroyCoroutine());
+        }
+
+        IEnumerator DelayedDestroyCoroutine()
+        {
+            yield return new WaitForSeconds(m_Duration);
+
+            Destroy(gameObject);
+        }
+    }
+}
