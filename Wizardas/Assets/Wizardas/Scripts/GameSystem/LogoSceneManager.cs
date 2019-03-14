68ff3f5d6da5907a43188856ba139a449c30cad6
Doo <doo11gms@gmail.com>
2019-03-14 12:31:03 +0900

    [fix]StartFromTitleにチェックを入れてもロゴが表示されるバグを修正

--- Wizardas/Assets/Wizardas/Scripts/GameSystem/LogoSceneManager.cs
+++ Wizardas/Assets/Wizardas/Scripts/GameSystem/LogoSceneManager.cs
@@ -12,10 +12,12 @@ namespace EllGames.Wiz.GameSystem
     public class LogoSceneManager : SerializedMonoBehaviour
     {
         [OdinSerialize] Profile.SystemProfile m_SystemProfile;
+        [OdinSerialize] GameObject m_LoadEvent;
         [OdinSerialize] GameObject m_SceneChangeEvent;
 
         private void Awake()
         {
+            m_LoadEvent.gameObject.SetActive(true);
             if (m_SystemProfile.StartFromTitle) m_SceneChangeEvent.gameObject.SetActive(true);
         }
     }
