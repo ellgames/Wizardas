e08d99ef2190860057293c278b5cc52eb84b3e73
Doo <doo11gms@gmail.com>
2019-03-14 14:18:28 +0900

    [fix]bugs

---
+++ Wizardas/Assets/Wizardas/Scripts/Event/DeleteSaveDataEvent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+using UnityEngine.Events;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+
+namespace EllGames.Wiz.Event
+{
+    public class DeleteSaveDataEvent : EventBase
+    {
+        [Title("Required")]
+        [OdinSerialize] Save.SaveHandler m_SaveHandler;
+
+        public override void Invoke()
+        {
+            base.Invoke();
+
+            m_SaveHandler.Delete();
+        }
+    }
+}
