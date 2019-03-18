1ca9ba6bdacf896dbb0f5b039f80aada4d7eeb1b
Doo <doo11gms@gmail.com>
2019-03-19 03:20:40 +0900

    [add]Opening scene

---
+++ Wizardas/Assets/Wizardas/Scripts/Audio/Fadeout.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+using UnityEngine.Events;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+
+namespace EllGames.Wiz.Audio
+{
+    public class Fadeout : SerializedMonoBehaviour
+    {
+        [OdinSerialize] AudioSource AudioSource { get; set; }
+	    [OdinSerialize] float Duration { get; set; }
+        [OdinSerialize] float Wait { get; set; }
+
+        private void Start()
+        {
+            StartCoroutine(Coroutine());
+        }
+
+        IEnumerator Coroutine()
+        {
+            yield return new WaitForSeconds(Wait);
+
+            var timeElapsed = 0f;
+
+            while (true)
+            {
+                timeElapsed += Time.deltaTime;
+                AudioSource.volume = 1f - timeElapsed / Duration;
+
+                if (timeElapsed >= Duration)
+                {
+                    break;
+                }
+                else
+                {
+                    yield return null;
+                }
+            }
+        }
+    }
+}
---
+++ Wizardas/Assets/Wizardas/Scripts/Camera/OpeningCamera.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+using UnityEngine.Events;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+
+namespace EllGames
+{
+    public class OpeningCamera : SerializedMonoBehaviour
+    {
+        [OdinSerialize] Transform FirstDestination { get; set; }
+        [OdinSerialize] float FirstDuration { get; set; } = 5f;
+        [OdinSerialize] Transform SecondRotation { get; set; }
+        [OdinSerialize] float SecondDuration { get; set; }
+        [OdinSerialize] float Wait { get; set; } = 5f;
+
+        private void Start()
+        {
+            iTween.MoveTo(gameObject, FirstDestination.position, FirstDuration);
+            StartCoroutine(DelayedRun());
+        }
+
+        IEnumerator DelayedRun()
+        {
+            yield return new WaitForSeconds(Wait);
+            iTween.RotateTo(gameObject, SecondRotation.eulerAngles, SecondDuration);
+        }
+    }
+}
